9a45ff7bfe0fcffc4945b7484ce7a3a969c5747c
Anisafauziah1 <fzhnsa@gmail.com>
2023-12-28 19:27:01 +0700

    Menambahkan onDetailClick pada HomeStatus

--- app/src/main/java/com/example/consumerestapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/screen/HomeScreen.kt
@@ -38,7 +38,7 @@ fun HomeStatus(
     retryAction: () -> Unit,
     modifier: Modifier = Modifier,
     onDeleteClick: (Kontak) -> Unit = {},
-
+    onDetailClick: (Int) -> Unit
 
 ){
     when (kontakUIState){
