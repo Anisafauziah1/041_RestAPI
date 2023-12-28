779660104865323aeaf8e06434615e3cbcf3dcc7
Anisafauziah1 <fzhnsa@gmail.com>
2023-12-28 19:29:25 +0700

    Menambahkan onDetailClick pada HomeStatus (when(kontakUIState))

--- app/src/main/java/com/example/consumerestapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/screen/HomeScreen.kt
@@ -45,7 +45,10 @@ fun HomeStatus(
         is KontakUIState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
         is KontakUIState.Success -> KontakLayout(
             kontak = kontakUIState.kontak,
-            modifier = modifier.fillMaxWidth()
+            modifier = modifier.fillMaxWidth(),
+            onDetailClick = {
+                onDetailClick(it.id)
+            },
         )
         is KontakUIState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
     }
